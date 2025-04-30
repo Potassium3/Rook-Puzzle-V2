74a74650981b41aeba59647f46fd76b05d0a51dc
Potassium3 <miles.plowman@gmail.com>
2025-04-30 07:31:39 +0000

    Update all

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ function show(puzzle, elem) {
     for (let y=0; y<GRIDSIZE; y++) {
         for (let x=0; x<GRIDSIZE; x++) {
             let full = Math.random() > 0.5;
+            let id = "s" + y + "-" + x;
             elem.innerHTML += 
             `<div class='square square-${full ? "full" : "empty"} square-${even ? "even" : "odd"}'>
-            <input class='square-radio' type='radio' name='squares' />
+            <a href="#${id}"><input id='${id}' class='square-check' type='radio' name='squares' /></a>
             <div class="square-cover"></div>
             </div>`;
             even = !even
