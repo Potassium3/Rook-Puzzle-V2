a91d616da94adfca82301999552215fe069af396
Potassium3 <miles.plowman@gmail.com>
2025-04-30 12:39:50 +0000

    Save

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const GRIDSIZE = 5; 
 
-function show(puzzle, elem) {
-    elem.style.gridTemplateColumns = "repeat("+GRIDSIZE+", auto)";
+function getHtml(puzzle) {
+    result = "";
     even = false
     for (let y=0; y<GRIDSIZE; y++) {
         for (let x=0; x<GRIDSIZE; x++) {
             let full = Math.random() > 0.5;
             let id = "s" + y + "-" + x;
-            elem.innerHTML += 
-            `<div class='square square-${full ? "full" : "empty"} square-${even ? "even" : "odd"}'>
-            <a href="#${id}"><input id='${id}' class='square-check' type='radio' name='squares' /></a>
-            <div class="square-cover"></div>
+            result.innerHTML += 
+            `<div id='${id}' class='square square-${full ? "full" : "empty"} square-${even ? "even" : "odd"}'>
+                <a href="#${id}">
+                    <input class='square-check' type='checkbox' /
+                </a>
+                <div class="square-cover"></div>
             </div>`;
             even = !even
         }
@@ -18,5 +20,11 @@ function show(puzzle, elem) {
             even = !even
         }
     }
+    return result;
+}
+
+function show(puzzle, elem) {
+    elem.style.gridTemplateColumns = "repeat("+GRIDSIZE+", auto)";
+    elem.innerHTML = getHtml(puzzle);
 }
 show(null, document.getElementById("grid-0"));
